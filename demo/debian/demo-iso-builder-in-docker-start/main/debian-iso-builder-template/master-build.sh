4c27fd3f6884aadfc7f794737c876b7d92aeabd1
samwhelp <samwhelp@users.noreply.github.com>
2026-01-20 21:34:57 +0800

    fix

--- demo/debian/demo-iso-builder-in-docker-start/main/debian-iso-builder-template/master-build.sh
+++ demo/debian/demo-iso-builder-in-docker-start/main/debian-iso-builder-template/master-build.sh
@@ -51,5 +51,4 @@ rm -rf "${REF_BUILD_DIR_PATH}"
 mkdir -p "${REF_PORT_DIR_PATH}"
 
 ## work for debootstrap in docker (--privileged)
-#podman run -it --rm --replace --privileged -v "${REF_PORT_DIR_PATH}:/port" -v "${REF_PLAN_DIR_PATH}:/opt/prj" -w "/opt/prj" --name "debian-iso-factory-13-run" "debian-iso-factory-13" "/opt/prj/src/build.sh"
-podman run -it --replace --privileged -v "${REF_PORT_DIR_PATH}:/port" -v "${REF_PLAN_DIR_PATH}:/opt/prj" -w "/opt/prj" --name "debian-iso-factory-13-run" "debian-iso-factory-13" "/opt/prj/src/build.sh"
+podman run -it --replace --privileged -v "${REF_PORT_DIR_PATH}:/port" -v "${REF_PLAN_DIR_PATH}:/opt/prj" -w "/opt/prj" --name "debian-iso-factory-13-run" "${REF_MASTER_BOX_DISTRO_NAME}" "/opt/prj/src/build.sh"
