61bc9c00ba76cd44cd98bf8d151d0d09b6c48380
samwhelp <samwhelp@users.noreply.github.com>
2026-01-20 21:15:25 +0800

    fix

--- demo/ubuntu/main/ubuntu-iso-builder-template/master-build.sh
+++ demo/ubuntu/main/ubuntu-iso-builder-template/master-build.sh
@@ -3,18 +3,43 @@
 
 
 
-## work for debootstrap in docker (--privileged)
-#podman run -it --rm --replace --privileged --name ubuntu-iso-factory-25.10-run ubuntu-iso-factory-25.10 /opt/src/build.sh
+##
+## ## Init
+##
+
+REF_CMD_FILE_NAME="$(basename "${0}")"
+REF_BASE_DIR_PATH="$(dirname "$(realpath "${0}")")"
 
 
 
 
 ##
-## ## create port dir
+## ## Path / Base
 ##
 
-mkdir -p ./port
+REF_PLAN_DIR_PATH="${REF_BASE_DIR_PATH}"
+REF_PORT_DIR_PATH="${REF_PLAN_DIR_PATH}/port"
+REF_BUILD_DIR_PATH="${REF_PLAN_DIR_PATH}/build"
+
+
+
+
+##
+## ## Clean
+##
+
+rm -rf "${REF_PORT_DIR_PATH}"
+rm -rf "${REF_BUILD_DIR_PATH}"
+
+
+
+
+##
+## ## Main
+##
+
+mkdir -p "${REF_PORT_DIR_PATH}"
 
 ## work for debootstrap in docker (--privileged)
-#podman run -it  -v ./port:/port --rm --replace --privileged --name ubuntu-iso-factory-25.10-run ubuntu-iso-factory-25.10 /opt/src/build.sh
-podman run -it  -v ./port:/port --replace --privileged --name ubuntu-iso-factory-25.10-run ubuntu-iso-factory-25.10 /opt/src/build.sh
+#podman run -it --rm --replace --privileged -v "${REF_PORT_DIR_PATH}:/port" -w "/opt/prj" --name "ubuntu-iso-factory-25.10-run" "ubuntu-iso-factory-25.10" "/opt/prj/src/build.sh"
+podman run -it --replace --privileged -v "${REF_PORT_DIR_PATH}:/port" -w "/opt/prj" --name "ubuntu-iso-factory-25.10-run" "ubuntu-iso-factory-25.10" "/opt/prj/src/build.sh"
